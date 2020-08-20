7f5ec4446a1666076f25de9fbde3b7015aa83d4f
AggieWB <usmaggie23@gmail.com>
2020-08-20 13:07:59 -0700

    Update to use ES6 syntax and keywords
    Remove extra spaces

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 'use strict';
-
-var express = require('express');
-var cors = require('cors');
+const express = require('express');
+const cors = require('cors');
 
 // require and use "multer"...
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (request, response) => {
+     response.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get('/hello',  (request, response) => {
+  response.json({greetings: "Hello, API"});
 });
 
 app.listen(process.env.PORT || 3000, function () {
