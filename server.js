d1f54e15b6d8197659bf66c20ee6c7108486af29
AggieWB <usmaggie23@gmail.com>
2020-08-21 10:21:10 -0700

    Require multer and remove comment instructions
    Change string concatenation to template literal

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 'use strict';
 const express = require('express');
 const cors = require('cors');
-
-// require and use "multer"...
-
+const multer = require('multer');
 const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', (request, response) => {
-     response.sendFile(process.cwd() + '/views/index.html');
-  });
+     response.sendFile(`${process.cwd()}/views/index.html`);
+});
 
 app.get('/hello',  (request, response) => {
   response.json({greetings: "Hello, API"});
